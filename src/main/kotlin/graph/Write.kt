417be9bbeec911f08ad10ee56ad14b22ca4183a2
Kostya <koreshin67@gmail.com>
2024-05-21 01:49:46 +0300

    feat: add cases for directed and undirected graphs

--- src/main/kotlin/graph/Write.kt
+++ src/main/kotlin/graph/Write.kt
@@ -3,28 +3,41 @@ package graph
 import java.io.BufferedWriter
 import java.io.FileWriter
 
-class Write<V>(private val graph: Graph<V>, name: String) {
-    private val svsEdgesList = graph.svsEdgesList()
+class Write<V>(private val graph: Graph<V>, private val name: String) {
     private val verticesCount = graph.verticesCount()
-    private val edgesCount = svsEdgesList.size
     private val writer = BufferedWriter(FileWriter("$name.csv"))
 
     init {
-        writeMainData()
-        writeVertices()
-        writeEdges()
+        writeData()
         writer.close()
     }
 
-    private fun writeMainData() {
+    private fun writeData() {
         val undirectedGraph = UndirectedGraph<V>()
         val directedGraph = DirectedGraph<V>()
+        var isGraphDirected = false
         when (graph::class) {
-            undirectedGraph::class -> writer.write("Undirected, ")
-            directedGraph::class -> writer.write("Directed, ")
+            undirectedGraph::class -> {
+                writer.write("Undirected, ")
+                writer.write("$verticesCount, ${graph.svsEdgesList().size}, ")
+            }
+            directedGraph::class -> {
+                writer.write("Directed, ")
+                var edgesCount = 0
+                val adjacencyList = graph.adjacencyList()
+                for (vertex in 0 until verticesCount) edgesCount += adjacencyList.outgoingEdgesCount(vertex)
+                writer.write("$verticesCount, $edgesCount, ")
+                isGraphDirected = true
+            }
         }
-        writer.write("est ves ili net, $verticesCount, $edgesCount")
+        when (graph.isWeighted()) {
+            true -> writer.write("Weighted")
+            else -> writer.write("Unweighted")
+        }
         writer.newLine()
+
+        writeVertices()
+        writeEdges(isGraphDirected)
     }
 
     private fun writeVertices() {
@@ -34,11 +47,25 @@ class Write<V>(private val graph: Graph<V>, name: String) {
         }
     }
 
-    private fun writeEdges() {
-        for (idEdge in 0 until edgesCount) {
-            val edge = svsEdgesList[idEdge]
-            writer.write("${edge.source()}, ${edge.target()}, ${edge.label()}, ${edge.weight()}")
-            writer.newLine()
+    private fun writeEdges(isGraphDirected: Boolean) {
+        when (isGraphDirected) {
+            true -> {
+                val adjacencyList = graph.adjacencyList()
+                for (vertex in 0 until verticesCount) {
+                    for (idEdge in 0 until adjacencyList.outgoingEdgesCount(vertex)) {
+                        val edge = adjacencyList.getEdge(vertex, idEdge)
+                        writer.write("${vertex}, ${edge.target()}, ${edge.label()}, ${edge.weight()}")
+                        writer.newLine()
+                    }
+                }
+            }
+            else -> {
+                val svsEdgesList = graph.svsEdgesList()
+                for (edge in svsEdgesList) {
+                    writer.write("${edge.source()}, ${edge.target()}, ${edge.label()}, ${edge.weight()}")
+                    writer.newLine()
+                }
+            }
         }
     }
 }
