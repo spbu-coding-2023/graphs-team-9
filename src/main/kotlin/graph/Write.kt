3727bb3517c2221d49a1508dc79e2f36181ec76f
Kostya <koreshin67@gmail.com>
2024-05-20 22:14:41 +0300

    feat: implement Write with FileWriter

--- src/main/kotlin/graph/Write.kt
+++ src/main/kotlin/graph/Write.kt
@@ -1,48 +1,43 @@
 package graph
 
 import java.io.BufferedWriter
-import java.io.FileOutputStream
-import java.io.OutputStream
+import java.io.FileWriter
 
 class Write<V>(private val graph: Graph<V>, name: String) {
     private val svsEdgesList = graph.svsEdgesList()
     private val verticesCount = graph.verticesCount()
     private val edgesCount = svsEdgesList.size
+    private val writer = BufferedWriter(FileWriter("$name.csv"))
 
     init {
-        FileOutputStream("${name}.csv").apply{ writeCsv() }
+        writeMainData()
+        writeVertices()
+        writeEdges()
+        writer.close()
     }
 
-    private fun OutputStream.writeCsv() {
-        val writer = bufferedWriter()
-        writeMainData(writer)
-        writeVertices(writer)
-        writeEdges(writer)
-        writer.flush()
-    }
-
-    private fun writeMainData(writer: BufferedWriter) {
+    private fun writeMainData() {
         val undirectedGraph = UndirectedGraph<V>()
         val directedGraph = DirectedGraph<V>()
         when (graph::class) {
-            undirectedGraph::class -> writer.write(""""UndirectedGraph<V>()", """)
-            directedGraph::class -> writer.write(""""DirectedGraph<V>()", """)
+            undirectedGraph::class -> writer.write("Undirected, ")
+            directedGraph::class -> writer.write("Directed, ")
         }
-        writer.write(""""est ves ili net", "$verticesCount", "$edgesCount"""")
+        writer.write("est ves ili net, $verticesCount, $edgesCount")
         writer.newLine()
     }
 
-    private fun writeVertices(writer: BufferedWriter) {
+    private fun writeVertices() {
         for (vertex in 0 until verticesCount) {
-            writer.write(""""${graph.vertexValue(vertex)}"""")
+            writer.write("${graph.vertexValue(vertex)}")
             writer.newLine()
         }
     }
 
-    private fun writeEdges(writer: BufferedWriter) {
+    private fun writeEdges() {
         for (idEdge in 0 until edgesCount) {
             val edge = svsEdgesList[idEdge]
-            writer.write(""""${edge.source()}", "${edge.target()}", "${edge.label()}", "${edge.weight()}"""")
+            writer.write("${edge.source()}, ${edge.target()}, ${edge.label()}, ${edge.weight()}")
             writer.newLine()
         }
     }
