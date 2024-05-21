e0b16181deb68989be0dbe0b6cc7a473b807ccc7
Nikita <nshchutskiy@gmail.com>
2024-05-21 20:34:49 +0300

    feat : add labels and weights views in edge view functions

--- src/main/kotlin/view/UndirectedEdgeView.kt
+++ src/main/kotlin/view/UndirectedEdgeView.kt
@@ -1,7 +1,10 @@
 package view
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -14,10 +17,10 @@ import viewModel.EdgeVM
 
 @Composable
 fun undirectedEdgeView(viewModel: EdgeVM) {
+    val coordinates = viewModel.computeCoordinates()
     Canvas(
         modifier = Modifier.fillMaxSize(),
     ) {
-        val coordinates = viewModel.computeCoordinates()
 
         if (!viewModel.isLoop) {
             val startCoordinates = coordinates.first
@@ -54,5 +57,43 @@ fun undirectedEdgeView(viewModel: EdgeVM) {
                 )
             }
         }
+    }
+    if (viewModel.labelVisibility) {
+        val modifier = if (!viewModel.isLoop) {
+            Modifier
+                .offset(
+                    coordinates.first.first * 0.8f + coordinates.second.first * 0.2f,
+                    coordinates.first.second * 0.8f + coordinates.second.second * 0.2f - 25.dp,
+                )
+                .background(Color.White)
+        }
+        else {
+            Modifier
+                .offset(coordinates.first.first - 40.dp, coordinates.first.second - 10.dp)
+                .background(Color.White)
+        }
+        Text(
+            modifier = modifier,
+            text = viewModel.edge.label(),
+        )
+    }
+    if (viewModel.weightVisibility) {
+        val modifier = if (!viewModel.isLoop) {
+            Modifier
+                .offset(
+                    coordinates.first.first * 0.8f + coordinates.second.first * 0.2f,
+                    coordinates.first.second * 0.8f + coordinates.second.second * 0.2f + 15.dp,
+                )
+                .background(Color.White)
+        }
+        else {
+            Modifier
+                .offset(coordinates.first.first, coordinates.first.second - 15.dp)
+                .background(Color.White)
+        }
+        Text(
+            modifier = modifier,
+            text = viewModel.edge.weight().toString()
+        )
     }
 }
