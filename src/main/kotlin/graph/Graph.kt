5b8fdb6cfb881bf55f672dcf2333f4787804f7af
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-05-23 10:42:40 +0000

    feat: implement shortestPathByDijkstra method in Graph class

--- src/main/kotlin/graph/Graph.kt
+++ src/main/kotlin/graph/Graph.kt
@@ -1,5 +1,7 @@
 package graph
 
+import algorithms.DijkstraAlgorithm
+
 abstract class Graph<V> {
     protected open val vertexValues: ArrayList<V> = arrayListOf()
     protected var vertexIndicesMap: HashMap<V, Int> = hashMapOf()
@@ -55,6 +57,37 @@ abstract class Graph<V> {
         start: V,
         end: V,
     ): MutableList<Int>?
+
+    fun shortestPathByDijkstra(
+        startVertexValue: V,
+        endVertexValue: V,
+    ): ArrayList<Int> {
+        require(!this.hasNegativeWeights) {
+            "Graph contains negative weights of edges"
+        }
+        val algo = DijkstraAlgorithm<V>(this.adjacencyList())
+        var startVertexIndex = -1
+        var endVertexIndex = -1
+        when (isAbleToAdd) {
+            true -> {
+                require(vertexIndicesMap[startVertexValue] != null || vertexIndicesMap[endVertexValue] != null) {
+                    "Vertices can not be null"
+                }
+                startVertexIndex = vertexIndicesMap.getValue(startVertexValue)
+                endVertexIndex = vertexIndicesMap.getValue(endVertexValue)
+            }
+            false -> {
+                for (vertexIndex in 0 until verticesCount()) {
+                    if (vertexValue(vertexIndex) == startVertexValue) startVertexIndex = vertexIndex
+                    if (vertexValue(vertexIndex) == endVertexValue) endVertexIndex = vertexIndex
+                }
+                require(startVertexIndex != -1 || endVertexIndex != -1) {
+                    "Vertices can not be null"
+                }
+            }
+        }
+        return algo.findShortestPath(startVertexIndex, endVertexIndex)
+    }
 
     abstract fun stronglyConnectedComponents(): ArrayList<ArrayList<Int>>
 
