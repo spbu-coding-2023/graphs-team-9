2a090ad149cdc5f23ae24e7095982d74645a61ed
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-05-28 17:22:19 +0300

    feat: reimplement recursive version of findCyclesForVertex method

--- src/main/kotlin/algorithms/CycleSearchAlgorithm.kt
+++ src/main/kotlin/algorithms/CycleSearchAlgorithm.kt
@@ -1,68 +1,56 @@
 package algorithms
 
-import graph.UndirectedGraph
-import java.util.Stack
+import graph.AdjacencyList
 
-class CycleSearchAlgorithm<V>(graph: UndirectedGraph<V>) {
-    private val vertexCount = graph.getVerticesCount()
-    private val adjacencyList = graph.getTheAdjacencyList()
+class CycleSearchAlgorithm(private val adjacencyList: AdjacencyList) {
+    private val vertexCount = adjacencyList.verticesCount()
+    private val cyclesArray = ArrayList<ArrayList<Int>>()
 
     enum class VertexStatus {
         NOT_VISITED,
+        PARTITION_VISITED,
         VISITED,
     }
 
-    fun printCycleForVertex(vertexIndex: Int) {
-        val (parentVertexIndicesArray, cycleEndVertexIndex) =
-            findCycleForVertex(vertexIndex)
-        val vertexIndicesForCycleArray: MutableList<Int> = mutableListOf()
-        var currVertexIndex = cycleEndVertexIndex
-
-        if (parentVertexIndicesArray == null) {
-            println("There are no cycles for a given vertex in the graph")
-            return
-        }
-        vertexIndicesForCycleArray.add(cycleEndVertexIndex)
-        do {
-            currVertexIndex = parentVertexIndicesArray[currVertexIndex]
-            vertexIndicesForCycleArray.add(currVertexIndex)
-        } while (cycleEndVertexIndex != parentVertexIndicesArray[currVertexIndex])
-        if (vertexIndicesForCycleArray.indexOf(vertexIndex) == -1) {
-            println("There are no cycles for a given vertex in the graph")
-            return
-        }
-        vertexIndicesForCycleArray.reverse()
-
-        val sb = StringBuilder()
-        vertexIndicesForCycleArray.forEach { sb.append("$it ") }
-        println(sb)
+    fun findCyclesForVertex(vertexIndex: Int): ArrayList<ArrayList<Int>> {
+        val visitedStatusArray = Array(vertexCount) { VertexStatus.NOT_VISITED }
+        val parentVertexArray = IntArray(vertexCount)
+        findCyclesWithDFS(vertexIndex, -1, visitedStatusArray, parentVertexArray, vertexIndex)
+        return cyclesArray
     }
 
-    private fun findCycleForVertex(vertexIndex: Int): Pair<Array<Int>?, Int> {
-        val stack = Stack<Int>()
-        val visitedOrNotArray = Array(vertexCount) { VertexStatus.NOT_VISITED }
-        val parentVertexIndicesArray = Array(vertexCount) { -1 } // initialize array with -1
-        var prevVertexIndex: Int = -1
-
-        stack.push(vertexIndex)
-        while (stack.isNotEmpty()) {
-            val currentVertexIndex = stack.pop()
-            for (neighbourEdge in adjacencyList[currentVertexIndex]) {
-                val neighbourVertexIndex = neighbourEdge.destinationVertexIndex
-                if (neighbourVertexIndex == prevVertexIndex) {
-                    continue
-                }
-                parentVertexIndicesArray[neighbourVertexIndex] = currentVertexIndex
-                if (visitedOrNotArray[neighbourVertexIndex] == VertexStatus.NOT_VISITED) {
-                    parentVertexIndicesArray[neighbourVertexIndex] = currentVertexIndex
-                    stack.push(neighbourVertexIndex)
-                } else if (visitedOrNotArray[neighbourVertexIndex] == VertexStatus.VISITED) {
-                    return parentVertexIndicesArray to neighbourVertexIndex   // neighbourVertexIndex is the end of cycle
-                }
-            }
-            prevVertexIndex = currentVertexIndex
-            visitedOrNotArray[currentVertexIndex] = VertexStatus.VISITED
+    private fun findCyclesWithDFS(
+        currentVertexIndex: Int,
+        parentVertexIndex: Int,
+        visitedStatusArray: Array<VertexStatus>,
+        parentArray: IntArray,
+        vertexIndex: Int,
+    ) {
+        if (visitedStatusArray[currentVertexIndex] == VertexStatus.VISITED) {
+            return
         }
-        return null to -1
+        if (visitedStatusArray[currentVertexIndex] == VertexStatus.PARTITION_VISITED) {
+            val currentCycleArray = ArrayList<Int>()
+            var currentVertexInCycleIndex = parentVertexIndex
+            currentCycleArray.add(currentVertexInCycleIndex)
+            while (currentVertexInCycleIndex != currentVertexIndex) {
+                currentVertexInCycleIndex = parentArray[currentVertexInCycleIndex]
+                currentCycleArray.add(currentVertexInCycleIndex)
+            }
+            if (vertexIndex !in currentCycleArray) return
+            currentCycleArray.reverse()
+            cyclesArray.add(currentCycleArray)
+            return
+        }
+        parentArray[currentVertexIndex] = parentVertexIndex
+        visitedStatusArray[currentVertexIndex] = VertexStatus.PARTITION_VISITED
+        for (outgoingEdgeNumber in 0 until adjacencyList.outgoingEdgesCount(currentVertexIndex)) {
+            val nextVertexIndex = adjacencyList.getEdge(currentVertexIndex, outgoingEdgeNumber).target()
+            if (nextVertexIndex == parentArray[currentVertexIndex]) {
+                continue
+            }
+            findCyclesWithDFS(nextVertexIndex, currentVertexIndex, visitedStatusArray, parentArray, vertexIndex)
+        }
+        visitedStatusArray[currentVertexIndex] = VertexStatus.VISITED
     }
 }
