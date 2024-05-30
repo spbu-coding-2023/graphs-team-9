161635bf52e00664461fc9eb92cc421bc0d0224b
Kostya <koreshin67@gmail.com>
2024-05-31 02:24:02 +0300

    fix: change checkboxes to radiobuttons

--- src/main/kotlin/view/SaveAsScreenView.kt
+++ src/main/kotlin/view/SaveAsScreenView.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Checkbox
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,9 +29,8 @@ fun saveAsScreen(
     onClick: () -> Unit,
     graphVM: GraphVM,
 ) {
-    var isGraphCheckboxCLicked by remember { mutableStateOf(false) }
-    var isAnalysisCheckboxCLicked by remember { mutableStateOf(false) }
     var isCSVButtonEnabled by remember { mutableStateOf(true) }
+    var isGraphButtonClicked by remember { mutableStateOf(true) }
 
     Column(
         modifier = Modifier.padding(15.dp),
@@ -48,20 +47,23 @@ fun saveAsScreen(
         )
 
         Row {
-            Checkbox(checked = isGraphCheckboxCLicked, onCheckedChange = { isGraphCheckboxCLicked = !isGraphCheckboxCLicked })
+            RadioButton(selected = isGraphButtonClicked, onClick = { isGraphButtonClicked = true })
             Text("graph", modifier = Modifier.padding(vertical = 10.dp))
         }
 
         Row {
-            Checkbox(checked = isAnalysisCheckboxCLicked, onCheckedChange = { isAnalysisCheckboxCLicked = !isAnalysisCheckboxCLicked })
+            RadioButton(selected = !isGraphButtonClicked, onClick = { isGraphButtonClicked = false })
             Text("analysis", modifier = Modifier.padding(vertical = 10.dp))
         }
 
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = {
-                if (isGraphCheckboxCLicked) Write(graphVM.graph, name)
-                if (isAnalysisCheckboxCLicked) WriteResultOfAnalysis(graphVM, name)
+                if (isGraphButtonClicked) {
+                    Write(graphVM.graph, name)
+                } else {
+                    WriteResultOfAnalysis(graphVM, name)
+                }
             },
             colors = ButtonDefaults.buttonColors(Color.White),
             enabled = isCSVButtonEnabled,
@@ -71,7 +73,7 @@ fun saveAsScreen(
             modifier = Modifier.fillMaxWidth(),
             enabled = neo4jRepository != null,
             onClick = {
-                if (isAnalysisCheckboxCLicked) {
+                if (!isGraphButtonClicked) {
                     neo4jRepository?.apply {
                         if (graphVM::class == UndirectedGraphVM::class) {
                             putAnalyzedGraphVM(graphVM, name, false)
