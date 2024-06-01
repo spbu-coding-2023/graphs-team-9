b102fd5805b7a479aef4340f450ed096daf2d9aa
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-06-01 14:44:45 +0300

    refactor: change column names

--- src/main/kotlin/storage/exposed/edges/EdgesTable.kt
+++ src/main/kotlin/storage/exposed/edges/EdgesTable.kt
@@ -5,9 +5,9 @@ import storage.exposed.graph.GraphsTable
 import storage.exposed.vertices.VerticesTable
 
 object EdgesTable : IntIdTable("Edges") {
-    val fromVertexIndex = reference("fromVertexIndex", VerticesTable)
-    val toVertexIndex = reference("toVertexIndex", VerticesTable)
-    val label = varchar("label", 100).nullable()
-    val weight = double("weight")
-    val relatedGraphID = integer("relatedGraph")
+    val fromVertexValue = reference("FromVertexValue", VerticesTable)
+    val toVertexValue = reference("ToVertexValue", VerticesTable)
+    val label = varchar("Label", 100).nullable()
+    val weight = double("Weight")
+    val relatedGraphID = integer("RelatedGraph")
 }
