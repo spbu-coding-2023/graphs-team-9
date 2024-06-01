fe28854ac3521543c06722508623d618443fab5f
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-06-01 14:39:29 +0300

    refactor: change column names

--- src/main/kotlin/storage/exposed/graph/GraphsTable.kt
+++ src/main/kotlin/storage/exposed/graph/GraphsTable.kt
@@ -3,6 +3,6 @@ package storage.exposed.graph
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object GraphsTable : IntIdTable("Graphs") {
-    val name = varchar("graphName", 100)
-    val type = varchar("type", 20)
+    val name = varchar("Name", 100)
+    val type = varchar("Type", 20)
 }
