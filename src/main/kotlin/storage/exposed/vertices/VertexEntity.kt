f3d181e83a24ae5614045b5fe4b861a18e48a0d9
Victoria Lutsyuk <victorialutsyuk.t@gmail.com>
2024-06-01 14:42:12 +0300

    feat: add column relatedGraphID to vertex entity

--- src/main/kotlin/storage/exposed/vertices/VertexEntity.kt
+++ src/main/kotlin/storage/exposed/vertices/VertexEntity.kt
@@ -3,8 +3,10 @@ package storage.exposed.vertices
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import storage.exposed.vertices.VerticesTable.integer
 
 class VertexEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<VertexEntity>(VerticesTable)
     var value by VerticesTable.index
+    var relatedGraphID by VerticesTable.relatedGraphID
 }
--- src/main/kotlin/storage/exposed/vertices/VerticesTable.kt
+++ src/main/kotlin/storage/exposed/vertices/VerticesTable.kt
@@ -3,6 +3,6 @@ package storage.exposed.vertices
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object VerticesTable : IntIdTable("Vertices") {
-    val index = varchar("value", 50)
-    val relatedGraphID = integer("relatedGraph")
+    val index = varchar("Value", 50)
+    val relatedGraphID = integer("RelatedGraph")
 }
